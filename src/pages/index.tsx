f2a9e8c84ff87f35efffb3346802e9467a9b1539
Ayyoub96 <ayyoub1996asri@gmail.com>
2023-05-21 03:24:04 +0200

    adding pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 
-import React, { useEffect, useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 
 const CoinMarketPage = () => {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&price_change_percentage=1h%2C24h%2C7d&locale=en');
+        const response = await fetch(
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${page}&price_change_percentage=1h%2C24h%2C7d&locale=en`
+        );
         const data = await response.json();
         setData(data);
       } catch (error) {
@@ -16,12 +20,21 @@ const CoinMarketPage = () => {
     };
 
     fetchData();
-  }, []);
+  }, [page]);
+
+  const handleNextPage = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    if (page > 1) {
+      setPage(prevPage => prevPage - 1);
+    }
+  };
 
   return (
-   
-  
-    <table className="mx-auto mt-4">
+    <div>
+      <table className="mx-auto mt-4">
       <thead className="border-t border-white border-opacity-20">
         <tr>
           <th className="bg-custom-orange px-10 py-2">#</th>
@@ -56,8 +69,23 @@ const CoinMarketPage = () => {
           </tr>
         ))}
       </tbody>
-    </table>
-    
+      </table>
+      <div className="flex justify-center mt-4">
+        <button
+          className="px-4 py-2 bg-custom-orange text-white rounded-md mr-2"
+          onClick={handlePrevPage}
+          disabled={page === 1}
+        >
+          Previous
+        </button>
+        <button
+          className="px-4 py-2 bg-custom-orange text-white rounded-md px-6"
+          onClick={handleNextPage}
+        >
+          Next
+        </button>
+      </div>
+    </div>
   );
 };
 
